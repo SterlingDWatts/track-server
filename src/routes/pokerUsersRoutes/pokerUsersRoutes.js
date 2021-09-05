431d97f520790756b5c6230814bd2a35aa66188c
Sterling Watts <sterling.watts@mindera.com>
2021-09-05 14:16:12 -0700

    Filter by time

--- src/routes/pokerUsersRoutes/pokerUsersRoutes.js
+++ src/routes/pokerUsersRoutes/pokerUsersRoutes.js
@@ -4,7 +4,7 @@ const PokerUser = require("../../models/PokerUser");
 const router = express.Router();
 
 router.get("/poker/users", async (req, res) => {
-  const users = await PokerUser.find();
+  const users = await PokerUser.find({ expDate: { $gte: new Date() } });
   res.status(201).send({ users });
 });
 
