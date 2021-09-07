bb23243393cd92d6eeffa0a83f7f96778cba36ba
Sterling Watts <sterling.watts@mindera.com>
2021-09-06 22:24:31 -0700

    Add websocket

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:3000", "http://192.168.86.49:3000"],
+    origin: ["http://localhost:3000", "https://story-point-poker.vercel.app/"],
   },
 });
 
